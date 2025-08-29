768f622927e7a8680b97400a0d54c99268d55626
CodyNinja1 <jailmanguy1@gmail.com>
2025-08-29 20:00:42 +0300

    Add OSS declaration

--- Twinkie/Modules/About/About.cpp
+++ Twinkie/Modules/About/About.cpp
@@ -1,20 +1,18 @@
 #include "About.h"
 
+#define LinkOSS(name, url) Text(name); SameLine(); TextLinkOpenURL(url)
+
 void AboutModule::Render()
 {
 	using namespace ImGui;
 	if (Begin(ICON_FK_INFO_CIRCLE " About Twinkie", &Enabled, ImGuiWindowFlags_AlwaysAutoResize))
 	{
+		SeparatorText("About");
 		Text(std::format("Twinkie for TrackMania Forever. Version {}", Versions.TwinkieVer).c_str());
 		Text(std::format("Lua Engine Version {}", Versions.LuaVer).c_str());
 		Text(std::format("Account type: {}", Twinkie->GetPayingAccountType() == TM::AccountType::Disconnected ? "Disconnected" : (Twinkie->GetPayingAccountType() == TM::AccountType::Nations ? "Nations" : "United")).c_str());
 		Text(std::format("Game type: {}", Twinkie->IsGameInstallUnited() ? "United" : "Nations").c_str());
 		Text("Made with love by jailman. <3");
-#ifdef BUILD_DEBUG
-		Separator();
-		Text("This is a debug copy, please report any bugs to the author, and do not share this with anyone.");
-#endif
-		Separator();
 
 		Text(ICON_FK_GITHUB); 
 		SameLine();
@@ -23,6 +21,17 @@ void AboutModule::Render()
 		Text(ICON_FK_DISCORD_ALT); 
 		SameLine();
 		TextLinkOpenURL("https://discord.gg/kRZ4MdCkVf");
+
+#ifdef BUILD_DEBUG
+		SeparatorText("Debug");
+		Text("This is a debug copy, please report any bugs to the author, and do not share this with anyone.");
+#endif
+
+		SeparatorText("OSS");
+		Text("Twinkie uses several open-source libraries. All of which are listed below.");
+		LinkOSS("dear imgui: ", "https://github.com/ocornut/imgui");
+		LinkOSS("mINI: ", "https://github.com/metayeti/mINI");
+		LinkOSS("IconFontCppHeaders: ", "https://github.com/juliettef/IconFontCppHeaders");
 	}
 	End();
 }
